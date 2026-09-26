5a1f1acfcc92fd2cdd190361aa6fd14e1558ef04
kushanavachaki-coder <kushanavachaki@gmail.com>
2026-09-26 20:30:11 +0530

    Fix Progress JSX closing tag

--- src/components/PlaceholderSection.tsx
+++ src/components/PlaceholderSection.tsx
@@ -358,7 +358,7 @@ export default function PlaceholderSection({
                   </div>
                 </div>
               </div>
-            </div>
+            </motion.div>
 
             {/* Syllabus Material Breakdown */}
             <motion.div
